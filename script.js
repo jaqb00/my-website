826f5e106ad43beade248c1d7fc26a3530460a9b
jaqb00 <jakubwes0416@gmail.com>
2025-12-02 23:13:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function menu(){
+    document.querySelector('nav').style.display = "flex";
+    document.getElementById("close_menu").style.display = "block";
+}
+
+function close_menu(){
+    document.querySelector('nav').style.display = "none";
+}
